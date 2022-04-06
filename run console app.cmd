1555f71bdae2eb1a9b349d863798dab057823d62
Niklas Lennerdahl <niklas.lennerdahl@gmail.com>
2022-04-06 19:54:40 +0200

    scripts

---
+++ run console app.cmd
@@ -0,0 +1,1 @@
+dotnet run --project ./src/NumberValidation.ConsoleApp/NumberValidation.ConsoleApp.csproj
---
+++ run web api.cmd
@@ -0,0 +1,1 @@
+dotnet run --project ./src/NumberValidation.API/NumberValidation.API.csproj
